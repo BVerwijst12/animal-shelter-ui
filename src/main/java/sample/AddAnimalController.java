97dbcc215f63fee2c335b1cb596cfa01adf962b0
bram verwijst <bram.verwijst12@gmail.com>
2019-03-12 11:01:11 +0100

    renames

--- src/main/java/sample/AddAnimalController.java
+++ src/main/java/sample/AddAnimalController.java
@@ -1,4 +1,24 @@
 package sample;
 
-public class AddAnimalController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddAnimalController implements Initializable {
+    public Button bt_AddAnimal;
+    public ChoiceBox SpeciesBox;
+    public ChoiceBox GenderBox;
+    public TextField tb_Name;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void AddAnimal(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -3,7 +3,23 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
-public class Controller{
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class Controller implements Initializable{
+
+    public Button bt_AddAnimal;
+    public ChoiceBox SpeciesBox;
+    public ChoiceBox GenderBox;
+    public TextField tb_Name;
+
+    public void AddAnimal(ActionEvent actionEvent) {
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
